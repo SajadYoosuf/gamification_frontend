9e27dc95df7060acff838705a7c9a7b36391649e
sajad yoosuf <sajadyoosuf24@gmail.com>
2025-11-04 18:07:52 +0530

    api url changed

--- lib/network/api_urls.dart
+++ lib/network/api_urls.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatf
 /// - On Android emulator, use 10.0.2.2 to reach host machine.
 /// - On other platforms default to localhost.
 final String base_url = (() {
-	if (kIsWeb) return 'http://localhost:4000';
-	if (defaultTargetPlatform == TargetPlatform.android) return 'http://10.0.2.2:4000';
-	return 'http://localhost:4000';
+	if (kIsWeb) return 'https://gamification-backend-wchb.onrender.com';
+	if (defaultTargetPlatform == TargetPlatform.android) return 'https://gamification-backend-wchb.onrender.com';
+	return 'https://gamification-backend-wchb.onrender.com';
 })();
