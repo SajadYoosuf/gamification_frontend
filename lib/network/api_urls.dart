444b2fde95ec6c04b0097993c90d8cae5b831c19
sajad yoosuf <sajadyoosuf24@gmail.com>
2025-11-06 22:52:53 +0530

    ds;lafjiogo

--- lib/network/api_urls.dart
+++ lib/network/api_urls.dart
@@ -2,5 +2,5 @@
 /// Dev-friendly base URL selection:
 /// - On Android emulator, use 10.0.2.2 to reach host machine.
 /// - On other platforms default to localhost.
-final String baseUrl ="http://localhost:4000";
-// final String baseUrl ="https://gamification-backend-wchb.onrender.com";
+// final String baseUrl ="http://localhost:4000";
+final String baseUrl ="https://gamification-backend-wchb.onrender.com";
